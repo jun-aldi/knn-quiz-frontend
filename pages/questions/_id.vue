0183292cebcbb1c573962bb16579a7eec814f5f7
juniaraldi <aldinugraha708@gmail.com>
2023-03-27 21:42:27 +0700

    question page

--- pages/questions/_id.vue
+++ pages/questions/_id.vue
@@ -1,36 +1,32 @@
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-        <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-        Quiz App
-      </a>
-      <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Kamu lebih suka belajar dimana?
-          </h1>
-          <div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-            <input id="bordered-radio-1" type="radio" value="" name="bordered-radio"
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="bordered-radio-1"
-              class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
-          </div>
-          <div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-            <input checked id="bordered-radio-2" type="radio" value="" name="bordered-radio"
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="bordered-radio-2"
-              class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
-          </div>
-        </div>
+  <section class="py-[50px] flex flex-col items-center justify-center px-4 items-center h-screen">
+    <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+      <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+      Dio Quiz
+    </a>
+    <form class="w-full card ">
+      <p>Apa yang akan kamu lakukan ketika bertemu singa?</p>
+      <div class="flex items-center mb-4">
+        <input id="default-radio-1" type="radio" value="" name="default-radio"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+        <label for="default-radio-1" class="ml-2 text-sm font-medium text-grey">Default
+          radio</label>
       </div>
-    </div>
+      <div class="flex items-center">
+        <input checked id="default-radio-2" type="radio" value="" name="default-radio"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 ">
+        <label for="default-radio-2" class="ml-2 text-sm font-medium text-grey">Checked
+          state</label>
+      </div>
+      <div class="flex justify-center mt-[14px]">
+        <NuxtLink :to="{ name: 'index' }" class="w-full btn btn-primary mx-2">Previous</NuxtLink>
+        <NuxtLink :to="{ name: 'questions-id' }" class="w-full btn btn-primary mx-2">Next</NuxtLink>
+      </div>
+    </form>
   </section>
 </template>
-
 <script>
 export default {
-  name: 'Question Page'
+  name: 'Quiz Page'
 }
 </script>
